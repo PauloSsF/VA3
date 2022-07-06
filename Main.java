4f04a916da8e4aa93abfcbf1a614b1b92d826e23
Paulo Sérgio de Souza Filho <54003071+PauloSsF@users.noreply.github.com>
2022-07-06 23:23:07 +0000

    primeiro commit

--- Main.java
+++ Main.java
@@ -11,12 +11,30 @@ public class Main {
     jogador.calcularTotal();
     jogador.retornarTesouroMaiorValor();
 
-    jogador.setarArma("Aerondith", 100, TipoArma.Espada);
+    jogador.setarArma("Arco Longo", 100, TipoArma.ArcoeFlecha);
     jogador.setarCastelo(100, 200);
 
     jogador.adicionarMasmorra("Templo da Perdição", "Nivel Médio", 1250);
     jogador.detalheMasmorra(0);
 
     jogador.castelo.receberAtaque(100);
+
+    Jogador jogador2 = new Jogador("Geralt", "Bruxo");
+
+    jogador.adicionarBau("Óleo", valor, 3);
+    jogador.adicionarBau("Elixir", valor, 2);
+    jogador.adicionarBau("Armadura rara", valor, 4);
+
+    jogador.calcularTotal();
+    jogador.retornarTesouroMaiorValor();
+
+    jogador.setarArma("Aerondith", 200, TipoArma.Espada);
+    jogador.setarCastelo(100, 300);
+
+    jogador.adicionarMasmorra("Covil da Striga", "Nivel alto", 2390);
+    jogador.detalheMasmorra(0);
+
+    jogador.castelo.receberAtaque(100);
   }
+
 }
